579226ecd8ab5ff476c958daedd4b319dbf60ad0
RETNE <109035506+R3TN3@users.noreply.github.com>
2023-12-01 17:57:24 +0900

    [Bronze I] Title: 단어 공부, Time: 80 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,20 +1,11 @@
-a = input().upper()
-b = sorted(set(a))
-b = ''.join(b)
+word = input().upper()
+word_list = list(set(word))
+cnt = []
 
-n = 0
-tmp_count = a.count(b[0])
-tmp = b[0]
+for i in word_list:
+  cnt.append(word.count(i))
 
-for i in range(1, len(b)):
-  if a.count(b[i]) > tmp_count:
-    tmp_count = a.count(b[i])
-    tmp = b[i]
-    n = 0
-  elif a.count(b[i]) == tmp_count:
-    n += 1
-
-if n != 0:
+if cnt.count(max(cnt)) > 1:
   print("?")
 else:
-  print(tmp)
+  print(word_list[cnt.index(max(cnt))])
