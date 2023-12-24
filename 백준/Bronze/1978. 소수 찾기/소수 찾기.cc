89b60eb99bfc729bc3786ade88520f2526530d9c
RETNE <109035506+R3TN3@users.noreply.github.com>
2023-12-24 17:50:07 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n, i, j;
+  int s[101];
+  int cnt = 0;
+
+  cin >> n;
+
+  for (i = 0; i < n; i++)
+    cin >> s[i];
+
+  for (i = 0; i < n; i++) {
+    int tmp = 0;
+    if (s[i] == 1)
+      continue;
+
+    for (j = 2; j < s[i]; j++) {
+      if (s[i] % j == 0) {
+        tmp += 1;
+        break;
+      }
+    }
+    if (tmp == 0)
+      cnt += 1;
+  }
+
+  cout << cnt;
+}
