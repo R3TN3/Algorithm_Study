9e2dbf1ba87f1ad920b0fd0d6bbabf7c3bb18f4a
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-01-04 12:36:03 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 396 ms, Memory: 53948 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -7,8 +7,7 @@ for i in range(n):
   a, b = list(map(int, sys.stdin.readline().split()))
   tmp.append([a, b])
 
-tmp.sort(key = lambda x : x[1])
-tmp.sort(key = lambda x : x[0])
+tmp.sort(key = lambda x : (x[0], x[1]))
 
 for i in tmp:
   print(i[0], i[1])
