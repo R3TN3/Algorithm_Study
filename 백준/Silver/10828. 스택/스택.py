8b9076bdd4d4b0271ce782378a34b89769924c50
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-01-05 11:20:39 +0900

    [Silver IV] Title: 스택, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(input())
+stack = []
+
+for i in range(n):
+  command = sys.stdin.readline().split()
+
+  if command[0] == "push":
+    stack.insert(0, command[1])
+  elif command[0] == "pop":
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack.pop(0))
+  elif command[0] == "size":
+    print(len(stack))
+  elif command[0] == "empty":
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+  elif command[0] == "top":
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack[0])
