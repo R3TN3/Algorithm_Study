29da780b0bfe302ae5033f6be6ca7812cd042c8b
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-01-09 14:51:08 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int fac(int num) {
+  int sum = 1;
+
+  for (int i = 1; i < num + 1; i++)
+    sum *= i;
+
+  return sum;
+}
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  int bin_coe = fac(n) / (fac(k) * fac(n - k));
+  cout << bin_coe << "\n";
+}
