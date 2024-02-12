9c0a0994bd25027d341a8bc25f8aa63720d54c0a
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-02-12 17:49:15 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  
+  int n;
+  cin >> n;
+  int a = 0, b = 1;
+
+  for (int i = 1; i < n; i++) {
+    int tmp = a;
+    a = b;
+    b += tmp;
+  }
+
+  cout << a << " " << b << "\n";
+}
