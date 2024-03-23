ea1fe3f073dd7c39b1f781fa988be413b732114c
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-03-23 13:37:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 56 ms, Memory: 5972 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compareRows(const int *row1, const int *row2) {
+  if (row1[1] == row2[1])
+    return row1[0] < row2[0];
+
+  return row1[1] < row2[1];
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  int **arr = new int *[n];
+
+  for (int i = 0; i < n; i++)
+    arr[i] = new int[2];
+
+  for (int i = 0; i < n; i++)
+    cin >> arr[i][0] >> arr[i][1];
+
+  sort(arr, arr + n, compareRows);
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i][0] << " " << arr[i][1] << "\n";
+
+  return 0;
+}
