e97b80e5aacab4672a428a1d3d2f646c750daa42
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-07-25 11:56:56 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    string a, b;
+    cin >> a >> b;
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == '6') a[i] = '5';
+    }
+
+    for (int i = 0; i < b.size(); i++) {
+        if (b[i] == '6') b[i] = '5';
+    }
+
+    cout << stoi(a) + stoi(b) << " ";
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == '5') a[i] = '6';
+    }
+
+    for (int i = 0; i < b.size(); i++) {
+        if (b[i] == '5') b[i] = '6';
+    }
+    
+    cout << stoi(a) + stoi(b) << "\n";
+    
+    return 0;
+}
