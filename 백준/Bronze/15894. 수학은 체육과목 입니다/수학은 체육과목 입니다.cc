cb996bcee2dfd0d6bc4082c1c58039c7e9be13c7
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-07-28 15:02:20 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    long long n;
+    cin >> n;
+
+    cout << n * 4 << "\n";
+    return 0;
+}
