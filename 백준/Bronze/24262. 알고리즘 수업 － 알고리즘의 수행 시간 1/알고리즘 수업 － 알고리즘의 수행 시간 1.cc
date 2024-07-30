221e66b1d33770763976b0754f52ef20abbfc256
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-07-31 00:54:44 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int n;
+    int cnt = 1, degree = 0;
+    cin >> n;
+
+    cout << cnt << "\n";
+    cout << degree << "\n";
+    
+    return 0;
+}
