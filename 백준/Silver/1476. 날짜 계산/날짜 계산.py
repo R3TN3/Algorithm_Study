a8d321470b0d6ca5fc31656d9a5963cc4eb43117
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-08-10 22:04:55 +0900

    [Silver V] Title: 날짜 계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,24 @@
+import sys
+
+e, s, m = map(int, sys.stdin.readline().split())
+cnt_e, cnt_s, cnt_m = 1, 1, 1
+cnt = 1
+
+while True:
+    if e == cnt_e and s == cnt_s and m == cnt_m:
+        break
+
+    else:
+        cnt += 1
+        cnt_e += 1
+        cnt_s += 1
+        cnt_m += 1
+
+        if cnt_e > 15:
+            cnt_e = 1
+        if cnt_s > 28:
+            cnt_s = 1
+        if cnt_m > 19:
+            cnt_m = 1
+
+sys.stdout.write(f"{cnt}\n")
