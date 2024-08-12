4250c2a5bf1f57dc2d059110430e1638e6d22bfe
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-08-12 12:01:55 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    int dp[n+1] = {0, };
+
+    for (int i = 2; i < n+1; i++) {
+        dp[i] = dp[i-1] + 1;
+
+        if (i % 3 == 0)
+            dp[i] = min(dp[i], dp[i/3] + 1);
+        if (i % 2 == 0)
+            dp[i] = min(dp[i], dp[i/2] + 1);
+    }
+
+    cout << dp[n] << "\n";
+    
+    return 0;
+}
