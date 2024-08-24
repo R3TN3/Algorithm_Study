63e6cbfa7538c10d738154d47c12eee45299ca60
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-08-24 23:04:40 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 696 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -3,7 +3,19 @@ import sys
 s = sys.stdin.readline().rstrip()
 q = int(sys.stdin.readline())
 
-for _ in range(q):
+arr = [[0] * 26]
+arr[0][ord(s[0]) - 97] = 1
+
+for i in range(1, len(s)):
+    arr.append(arr[-1][:])
+    arr[i][ord(s[i]) - 97] += 1
+
+
+for i in range(q):
     a, l, r = map(str, sys.stdin.readline().split())
     l, r = int(l), int(r)
-    sys.stdout.write(f"{s[l:r+1].count(a)}\n")
+
+    if l == 0:
+        sys.stdout.write(f"{arr[r][ord(a) - 97]}\n")
+    else:
+        sys.stdout.write(f"{arr[r][ord(a) - 97] - arr[l-1][ord(a) - 97]}\n")
