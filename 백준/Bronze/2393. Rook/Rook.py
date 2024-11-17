f1e9c324fd11847ca080b5206b8e0db50300e395
RETNE <109035506+R3TN3@users.noreply.github.com>
2024-11-18 00:28:31 +0900

    [Bronze V] Title: Rook, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.py
@@ -0,0 +1,15 @@
+import sys
+
+sys.stdout.write("  ___  ___  ___\n")
+sys.stdout.write("  | |__| |__| |\n")
+sys.stdout.write("  |           |\n")
+sys.stdout.write("   \\_________/\n")
+sys.stdout.write("    \\_______/\n")
+sys.stdout.write("     |     |\n")
+sys.stdout.write("     |     |\n")
+sys.stdout.write("     |     |\n")
+sys.stdout.write("     |     |\n")
+sys.stdout.write("     |_____|\n")
+sys.stdout.write("  __/       \\__\n")
+sys.stdout.write(" /             \\\n")
+sys.stdout.write("/_______________\\\n")
